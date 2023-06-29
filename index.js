620e89f98b99766dc5a395cd2a88af04b69473d4
Nato Pedroso <dj_dnatob@hotmail.com>
2023-06-29 10:59:17 -0300

    pilot

--- index.js
+++ index.js
@@ -7,23 +7,25 @@ const config = require("./config.js").config;
 
 // Schedule a cron job to run every day at midnight (00:00)
 
-// cron.schedule(config.cron, async () => {
-try {
-  console.log("Trying to backup", config);
+cron.schedule(config.cron, async () => {
+  try {
+    console.log("Trying to backup", config);
 
-  const backupFileName = `${config.database}_${new Date().getDay()}.sql`;
-  const backupFilePath = `./backups/${backupFileName}`;
-  const backupCommand = `mysqldump --user=${config.user} --password=${config.password} --host=${config.host} --port=${config.port} ${config.database} > ${backupFilePath}`;
-  const exportProcess = exec(backupCommand);
+    const sufix = config.loopMode === "DAILY" ? new Date().getDate() : config.loopMode === "WEEKLY" ? new Date().getDay() + 1 : new Date().getMonth() + 1;
 
-  exportProcess.on("exit", (code) => {
-    if (code === 0) {
-      console.log(`Database backup created successfully: ${backupFileName}`);
-    } else {
-      console.error(`Error creating database backup. Exit code: ${code}`);
-    }
-  });
-} catch (error) {
-  console.error("Error creating database backup:", error);
-}
-// });
+    const backupFileName = `${config.database}_${sufix}.sql`;
+    const backupFilePath = `./backups/${backupFileName}`;
+    const backupCommand = `mysqldump --user=${config.user} --password=${config.password} --host=${config.host} --port=${config.port} ${config.database} > ${backupFilePath}`;
+    const exportProcess = exec(backupCommand);
+
+    exportProcess.on("exit", (code) => {
+      if (code === 0) {
+        console.log(`Database backup created successfully: ${backupFileName}`);
+      } else {
+        console.error(`Error creating database backup. Exit code: ${code}`);
+      }
+    });
+  } catch (error) {
+    console.error("Error creating database backup:", error);
+  }
+});
